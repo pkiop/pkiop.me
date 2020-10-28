9ae57f08fccbdd865d26b2df6200b6c907358865
pkiop <pkiop@naver.com>
2020-10-29 00:18:39 +0900

    chore: webpack 설정 변경

--- src/Backend/webpack.config.js
+++ src/Backend/webpack.config.js
@@ -1,37 +1,33 @@
-import path from 'path';
-import dotenv from 'dotenv';
-dotenv.config();
-export default (webpackEnv) => {
-  return {
-    mode: 'development', // production
-    devtool: 'eval',     // hidden-source-map => 그냥 source-map하면 개발자 도구에 코드 다 노출됨
-    resolve: {
-      extensions: ['.jsx', '.js', '.tsx','.ts']
-    },
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const babelConfig = require('./babel.config.js');
 
-    entry: {
-      app: './client'
-    },
-
-    module: {
-      rules: [{
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-      }]
-    },
-
-    plugins: [
-
+module.exports = {
+  target: 'node',
+  entry: [
+    path.resolve(__dirname, 'app.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.js',
+  },
+  externals: [
+    nodeExternals(),
+  ],
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelConfig,
+        },
+      },
     ],
-
-    output: webpackEnv === 'development'
-    ? {
-      publicPath: '/',
-    }
-    : {
-      path: path.resolve(__dirname, '../../dist/public'),
-      filename: '[name].bundle.js',
-    }, 
-    target: 'node',
-  }
-}
+  },
+};
