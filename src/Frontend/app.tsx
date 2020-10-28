de6cb70ce676c910fb9e3d3d62edead494ad9f2c
pkiop <pkiop@naver.com>
2020-10-29 00:24:12 +0900

    feat: first fe app

--- src/Frontend/app.tsx
+++ src/Frontend/app.tsx
@@ -1,32 +1,11 @@
 import * as React from 'react';
-import { Component, FC } from 'react';
-import { connect,useSelector, useDispatch } from 'react-redux';
-import { logIn, logOut, ThunkDispatch } from './actions/user';
+import { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const App: FC = () => {
-  const { isLoggingIn, data} = useSelector<RootState, UserState>((state) => state.user);
-  const dispatch = useDispatch();
-
-  const onClick = () => {
-    dispatch(logIn({
-      id: 'zerocho',
-      password: '비밀번호',
-    }));
-  }
-
-  const onLogout = () => {
-    dispatch(logOut());
-  }
     return (
       <div>
-        {isLoggingIn
-          ? <div>로그인 중</div>
-          : data
-            ? <div>{data.nickname}</div>
-            : '로그인 해주세요'}
-        {!data
-          ? <button onClick={onClick}>로그인</button>
-          : <button onClick={onLogout}>로그아웃</button>}
+        <h1>first app</h1> 
       </div>
     )
 
