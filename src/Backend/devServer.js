e246bdcff5ddfef9ed6c6a1e955c5ea0aa845401
pkiop <pkiop@naver.com>
2020-10-29 22:41:13 +0900

    feat: 서버 개발용으로 세팅
    - TODO: 배포용도 배포시 고려

--- src/Backend/devServer.js
+++ src/Backend/devServer.js
@@ -1,15 +1,14 @@
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../frontend/webpack.config';
+import webpackConfig from '../Frontend/webpack.config';
 
-const webpackDevConfig = webpackConfig('development');
-const compiler = webpack(webpackDevConfig);
+const compiler = webpack(webpackConfig);
 const hotModule = WebpackHotMiddleware(compiler);
 
 const useDevServer = (app) => {
   app.use(WebpackDevMiddleware(compiler, {
-    publicPath: webpackDevConfig.output.publicPath,
+    publicPath: webpackConfig.output.publicPath,
   }));
   app.use(hotModule);
 };
