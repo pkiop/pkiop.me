131ad40c06ed6d06cb843930737d7bfdfa8183ff
pkiop <pkiop@naver.com>
2020-10-30 17:24:39 +0900

    feat: App 컴포넌트에 라우터 배치

--- src/Frontend/Components/App.tsx
+++ src/Frontend/Components/App.tsx
@@ -1,11 +1,40 @@
-import * as React from 'react';
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import HeaderBar from '@Components/HeaderBar';
+import Portfolio from '@Components/Portfolio';
+import Home from '@Components/Home';
+import Post from '@Components/PostComponent';
+import TIL from '@Components/TIL';
+import LM from '@Components/LM';
 import { FC } from 'react';
 
 const App: FC = () => {
     return (
-      <div>
-        <h1>first app</h1> 
-      </div>
+      <BrowserRouter>        
+        <HeaderBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/post">
+            <Post />
+          </Route>
+          <Route path="/TIL">
+            <TIL />
+          </Route>
+          <Route path="/LM">
+            <LM />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     )
 };
 
