4cf43ed7cd7fff04f14deb86d89d515b831240fe
pkiop <pkiop@naver.com>
2020-10-30 16:54:33 +0900

    feat: 라우팅 테스트 용 Sample Component 생성

---
+++ src/Frontend/Components/LM/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Fragment, FC, useState, useEffect } from 'react';
+import styled from "styled-components";
+import {
+  Link
+} from 'react-router-dom';
+
+const App: FC = () => {
+
+  return (
+    <>
+      <h1>LM</h1> 
+    </>
+  )
+};
+
+export default App;
