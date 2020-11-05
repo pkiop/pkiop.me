f78f11e9d2a6514224b84e81cab36dfe0e64f63d
pkiop <pkiop@naver.com>
2020-11-06 07:28:29 +0900

    fix: rem단위로 수정

--- src/Frontend/styles/theme.ts
+++ src/Frontend/styles/theme.ts
@@ -1,8 +1,11 @@
 
-
-
 export const theme = {
+  mainFontSize: 16,
+
   mainColor: '#2E4B73',
   subColor: '#83BFCD',
-  smallWidth: '768px'
+  smallWidth: '48rem',
+
+  headerbarHeight: '3.875rem',
+  headerbarContentHeight: '3.75rem',
 };
