5213105ac77889927265ec9893855bee991e0d9a
pkiop <pkiop@naver.com>
2020-11-06 07:29:16 +0900

    feat: Cover이미지 컴포넌트 작성

--- src/Frontend/Components/Skills/index.tsx
+++ src/Frontend/Components/Skills/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Fragment, FC, useState, useEffect } from 'react';
 import styled from "styled-components";
+import coverImg from '@Images/Cover.jpg';
 import {
   Link
 } from 'react-router-dom';
 
 const Main = styled.div`
-  height: 1000px;
+  height: ${window.innerHeight}px;
   color: blue;
 ` 
-const App: FC = () => {
 
+const Cover = styled.img`
+  width:100%;
+  height:100%;
+  object-fit: cover;
+`
+
+const App: FC = () => {
   return (
     <Main>
-      <h1>Skills</h1> 
+      <Cover src={coverImg}></Cover>
     </Main>
   )
 };
