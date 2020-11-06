1d2b7676315b5f0ceceaa76928013ed27d3a1562
pkiop <pkiop@naver.com>
2020-11-06 19:58:25 +0900

    fix: remToPixel export default -> export

--- src/Frontend/utils/remToPixel.ts
+++ src/Frontend/utils/remToPixel.ts
@@ -1,8 +1,6 @@
 import { theme } from '@Styles/theme';
 
-const remToPixel = (remValue: string) : number => {
+export const remToPixel = (remValue: string) : number => {
   const remNum = parseInt(remValue.substr(0, remValue.length - 3));
   return remNum * theme.mainFontSize;
 }
-
-export default remToPixel;
