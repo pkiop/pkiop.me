732399ca4fc7b51bec7857e6425c14e560308c38
pkiop <pkiop@naver.com>
2020-11-06 18:05:20 +0900

    feat: 일정 이상 스크롤 시 이동하도록 패턴 구현

--- src/Frontend/Components/About/index.tsx
+++ src/Frontend/Components/About/index.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import AboutMe from '@Components/AboutMe';
 import Skills from '@Components/Skills';
+import { getScrollTop } from '@Hooks/getScroll';
 
 const Main = styled.div`
 `
@@ -13,14 +14,19 @@ const Main = styled.div`
 const App: FC = () => {
   const [aboutMeSize, setAboutMeSize] = useState<Array<number>>([0,0]);
   const [skillsSize, setSkillsSize] = useState<Array<number>>([0,0]);
+  const scrollTop = getScrollTop();
 
   useEffect(() => {
     console.log("*** in about ***");
     console.log(`
       aboutmesize : ${aboutMeSize}    
       skillssize : ${skillsSize}    
+      scrollTop : ${scrollTop}    
     `)
-  }, [aboutMeSize, skillsSize])
+    if(scrollTop > 50) {
+      window.scrollTo(0, 1000);
+    }
+  }, [aboutMeSize, skillsSize, scrollTop])
   return (
     <Main>
       <AboutMe setSize={setAboutMeSize}/>
