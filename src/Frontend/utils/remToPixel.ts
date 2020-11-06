4cca2b2660709215c6c91431b409da567e29d4a4
pkiop <pkiop@naver.com>
2020-11-06 20:43:20 +0900

    fix: parseInt로 하면 rem소숫점 날아가는 것 수정

--- src/Frontend/utils/remToPixel.ts
+++ src/Frontend/utils/remToPixel.ts
@@ -1,6 +1,7 @@
 import { theme } from '@Styles/theme';
 
 export const remToPixel = (remValue: string) : number => {
-  const remNum = parseInt(remValue.substr(0, remValue.length - 3));
+  const remNum = Number(remValue.substr(0, remValue.length - 3));
+  
   return remNum * theme.mainFontSize;
 }
