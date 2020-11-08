413794c63f362ef0029069a9498edf9742d516d7
pkiop <pkiop@naver.com>
2020-11-08 23:32:27 +0900

    feat: 가속도 효과 준 스크롤 setIntervalu로 구현

--- src/Frontend/Components/About/index.tsx
+++ src/Frontend/Components/About/index.tsx
@@ -20,17 +20,50 @@ const App: FC = () => {
   const [ gotoSize, setGotoSize] = useState<Array<number>>([0,0]);
   const [ isAutoScrolled, setIsAutoScrolled ] = useState<boolean>(false);
   const [ isAnimatedGoto, setIsAnimatedGoto ] = useState<boolean>(false);
+  const mainComponent = useRef<HTMLDivElement>(null);
   const scrollY = getScrollY();
+  const [ running, setRunning ] = useState<boolean>(false);
 
   useEffect(() => {
+    const htmlTag = mainComponent.current!.closest('body');
     const skillToTop = aboutMeSize[1] + remToPixel(theme.headerbarHeight);
-    if(scrollY > 50 && !isAutoScrolled) {
-      // window.scrollTo(0, skillToTop);
-      setIsAutoScrolled(true);
+    if(scrollY > 50 && !isAutoScrolled && running === false) {
+      if(running === false) {
+        setRunning(true);
+        let time = skillToTop;
+        setInterval(() => {
+          (() => {
+            if(time !== 0) {
+              
+              window.scrollTo(0, skillToTop - time);
+              if(time > 500) {
+                time -= 40;  
+              }
+              if(time > 100) {
+                time -= 10;
+              }
+              if(time > 30) {
+                time -= 2;
+              }
+              time--;
+              if(time === 0) {
+                setIsAutoScrolled(true);
+                setRunning(false);
+              }
+            }
+          })();
+        }, 1);
+
+      }
     }
-    if(scrollY < skillToTop && isAutoScrolled) {
-      // window.scrollTo(0, 0);
+    console.log(scrollY);
+    console.log("st: ", skillToTop);
+    console.log("isAuto ", isAutoScrolled);
+    console.log("running", running);
+    if(scrollY < skillToTop - 10 && isAutoScrolled && running === false) {
+      window.scrollTo(0, 0);
       setIsAutoScrolled(false);
+      setRunning(false);
     }
 
     const GotoQuarter = skillToTop + skillsSize[1] - Math.floor(gotoSize[1] * 0.8);
@@ -40,7 +73,7 @@ const App: FC = () => {
   }, [aboutMeSize, scrollY])
 
   return (
-    <Main>
+    <Main ref={mainComponent}>
       <AboutMe setSize={setAboutMeSize}/>
       <Skills setSize={setSkillsSize}/>
       <Goto setSize={setGotoSize} isAnimated={isAnimatedGoto}/>
