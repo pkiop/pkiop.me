54cd867041dfbc2b655277b0af4297ca2f086b23
pkiop <pkiop@naver.com>
2020-11-12 07:41:05 +0900

    hotfix: mt 타입에러 수정

--- src/Frontend/Components/TextSlide/text.tsx
+++ src/Frontend/Components/TextSlide/text.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Fragment, FC, useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
 
+interface mainProps {
+  mt: number,
+  mb: number,
+}
+
 const Main = styled.p`
   max-width:80%;
   font-size:36px; 
   color:#333; 
   transition:color .5s;
-  margin-top: ${props => props.mt}px;
-  margin-bottom: ${props => props.mb}px;
+  margin-top: ${(props:mainProps) => props.mt}px;
+  margin-bottom: ${(props:mainProps) => props.mb}px;
   white-space: normal;
   word-wrap: break-word;
 `
