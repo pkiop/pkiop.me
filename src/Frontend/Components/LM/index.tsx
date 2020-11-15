51b9859cdf8bdb59e40a6921b9cf404a246e0a60
pkiop <pkiop@naver.com>
2020-11-15 12:04:22 +0900

    fix: Test를 위한 LM 컴포넌트에 subtitle props추가

--- src/Frontend/Components/LM/index.tsx
+++ src/Frontend/Components/LM/index.tsx
@@ -2,9 +2,15 @@ import React, {
   FC,
 } from 'react';
 
-const App: FC = () => (
+interface Props {
+  subTitle: string,
+}
+
+const App: FC<Props> = ({ subTitle }) => (
+
   <>
     <h1>LM</h1>
+    <h2>subTitle : {subTitle}</h2>
   </>
 );
 
