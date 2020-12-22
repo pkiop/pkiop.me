4816e6dcaa03bbaa090db2aba9cd26053dd291c5
pkiop <pkiop@naver.com>
2020-12-23 00:42:36 +0900

    Feat:  Portfolio 와 main 같은 페이지 렌더링하도록 수정

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           </Route>
           <Route path="/portfolio">
             <HeaderBar />
-            <Portfolio />
+            <About />
           </Route>
           <Route path="/post">
             <HeaderBar />
