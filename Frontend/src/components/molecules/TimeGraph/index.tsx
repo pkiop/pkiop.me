b4174a0058167f774f9819776acfe5785c1fe479
pkiop <pkiop@naver.com>
2020-12-25 10:56:09 +0900

    Feat: Event갯수따라 Line 적절한 위치 추가

--- Frontend/src/components/molecules/TimeGraph/index.tsx
+++ Frontend/src/components/molecules/TimeGraph/index.tsx
@@ -13,18 +13,27 @@ interface IProps {
   events: IEvent[],
   width: number,
   height: number,
+  diagonalXDirectionLength?: number,
 }
-function App({ events }: IProps) {
-  console.log('Event : ', events);
 
+function App({
+  events, width, height, diagonalXDirectionLength = 50,
+}: IProps) {
+  const eventLines = events.map((event: IEvent, idx: number) => {
+    const xOffset = (idx === 0 ? 0 : (width - diagonalXDirectionLength) / idx);
+    console.log(xOffset);
+    return (<S.Line x1={xOffset} x2={xOffset + diagonalXDirectionLength}
+      y1={height } y2={height - diagonalXDirectionLength} />);
+  });
+  console.log(eventLines);
   const svgImage = (
-    <svg width={500} height={100}>
-      <S.Line x1="50" y1="50" x2="400" y2="50" />
+    <svg width={width} height={height + 5}>
+      {eventLines}
+      <S.Line x1={0} y1={height} x2={width - diagonalXDirectionLength} y2={height} />
     </svg>
   );
   return (
     <S.Main>
-      <div>이벤트들</div>
       {svgImage}
     </S.Main>
   );
