44f122b3850a23fbff38f8011e77211b06e98862
pkiop <pkiop@naver.com>
2021-01-26 23:44:24 +0900

    Feat: Add postwrapper style

---
+++ Frontend/src/pages/Post/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PostWrapper = styled.div`
+  position: relative;
+  width: 400px;
+  box-sizing: border-box;
+  img {   
+    max-width: 100%;
+  }
+`;
+
+export default {};
