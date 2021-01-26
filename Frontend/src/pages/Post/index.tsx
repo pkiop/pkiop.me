50b9e5d9998caf361e909904b422170d892ad4c6
pkiop <pkiop@naver.com>
2021-01-26 23:43:22 +0900

    Feat: Add md file has html parsing

--- Frontend/src/pages/Post/index.tsx
+++ Frontend/src/pages/Post/index.tsx
@@ -1,8 +1,25 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { Remarkable } from 'remarkable';
+import * as S from './style';
 
-const md = new Remarkable();
+const md = new Remarkable({
+  html: true, // Enable HTML tags in source
+  xhtmlOut: true, // Use '/' to close single tags (<br />)
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  langPrefix: 'language-', // CSS language prefix for fenced blocks
+
+  // Enable some language-neutral replacement + quotes beautification
+  typographer: false,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+  quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed
+  highlight(/* str, lang */) { return ''; },
+});
 
 function Post() {
   const [data, setData] = useState<string>('');
@@ -23,9 +40,7 @@ function Post() {
   }
   const codes = md.render(data);
   return (
-    <>
-      <div dangerouslySetInnerHTML={ { __html: codes } }/>
-    </>
+    <S.PostWrapper dangerouslySetInnerHTML={ { __html: codes } } />
   );
 }
 
