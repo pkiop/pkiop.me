2314827af1ec9475c1f97eebef72335c0e82ecab
Sangshin Park <34783156+pkiop@users.noreply.github.com>
2021-03-25 23:34:17 +0900

    Update remToPixel.ts

--- Frontend/src/utils/remToPixel.ts
+++ Frontend/src/utils/remToPixel.ts
@@ -1,7 +1,5 @@
 import theme from 'styles/theme';
 
-export const dummy = 1;
-
 export const remToPixel = (remValue: string | number) : number => {
   if (typeof remValue === 'string') {
     const remNum = Number(remValue.substr(0, remValue.length - 3));
